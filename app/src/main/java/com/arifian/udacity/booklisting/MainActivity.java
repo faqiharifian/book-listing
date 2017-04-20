749f349c9143bec62f64603ef5a0d34d63cf842f
Faqih Arifian <faqiharifianaji@gmail.com>
2017-04-21 05:32:21 +0700

    main activity complete

--- app/src/main/java/com/arifian/udacity/booklisting/MainActivity.java
+++ app/src/main/java/com/arifian/udacity/booklisting/MainActivity.java
@@ -1,13 +1,31 @@
 package com.arifian.udacity.booklisting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchView = (SearchView) findViewById(R.id.search_book);
+        searchView.onActionViewExpanded();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                startActivity(new Intent(MainActivity.this, SearchResultActivity.class));
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
